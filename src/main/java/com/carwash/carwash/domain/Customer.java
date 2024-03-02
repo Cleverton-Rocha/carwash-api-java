d6df155bcf4d54220640281042da49068679261a
ultr4 <cl.rch09@icloud.com>
2024-03-01 23:13:13 -0300

    :sparkles: feat: Add createCustomer method

--- src/main/java/com/carwash/carwash/domain/Customer.java
+++ src/main/java/com/carwash/carwash/domain/Customer.java
@@ -2,6 +2,8 @@ package com.carwash.carwash.domain;
 
 import java.util.List;
 
+import com.carwash.carwash.model.Customer.CustomerRequest;
+
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -10,9 +12,10 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 import lombok.Data;
 
-@Table(name = "Customer")
+@Table(name = "Customer", uniqueConstraints = { @UniqueConstraint(columnNames = "email") })
 @Entity
 @Data
 public class Customer {
@@ -23,15 +26,22 @@ public class Customer {
   @Column(nullable = false)
   private String name;
 
-  @Column(nullable = false )
+  @Column(nullable = false)
   private String address;
 
   @Column(nullable = false)
-  private Integer phone;
+  private String phone;
 
-  @Column(nullable = false)
+  @Column(nullable = false, unique = true)
   private String email;
 
   @OneToMany(mappedBy = "customer", cascade = CascadeType.ALL)
   private List<Car> cars;
+
+  public void createCustomer(CustomerRequest request) {
+    this.name = request.name();
+    this.address = request.address();
+    this.phone = request.phone();
+    this.email = request.email();
+  }
 }

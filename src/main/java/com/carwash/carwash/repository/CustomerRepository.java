35c7bf01872f8e3720b8a6e7ad2e90eec4b867dc
ultr4 <cl.rch09@icloud.com>
2024-03-01 23:14:58 -0300

    :sparkles: feat: Add findByEmail custom method

--- src/main/java/com/carwash/carwash/repository/CustomerRepository.java
+++ src/main/java/com/carwash/carwash/repository/CustomerRepository.java
@@ -1,5 +1,7 @@
 package com.carwash.carwash.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,5 @@ import com.carwash.carwash.domain.Customer;
 
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
-  
+  Optional<Customer> findByEmail(String email);
 }

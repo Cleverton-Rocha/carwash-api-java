79c76bf2591a6e1310bab5d85c40e6aa9fe9d384
ultr4 <cl.rch09@icloud.com>
2024-03-01 23:13:32 -0300

    :sparkles: feat: Create CustomerNotFound exception

---
+++ src/main/java/com/carwash/carwash/exception/CustomerNotFoundException.java
@@ -0,0 +1,7 @@
+package com.carwash.carwash.exception;
+
+public class CustomerNotFoundException extends RuntimeException {
+  public CustomerNotFoundException(String message) {
+    super(message);
+  }
+}
